ff1eb7e5cbe7a1ac51ded33816455a1098ac4c15
Ashwin Chatterji <ashwinath@hotmail.com>
2018-10-11 23:20:36 +0800

    added typed js

--- frontend/src/components/LandingText.jsx
+++ frontend/src/components/LandingText.jsx
@@ -1,13 +1,38 @@
 import React, { Component } from 'react';
+import Typed from "typed.js";
 import "./LandingText.css";
 
 class LandingText extends Component {
+    componentDidMount() {
+        const strings = [
+            "hello world",
+            "goodbye world",
+        ];
+        const options = {
+            strings,
+            typeSpeed: 50,
+            backSpeed: 50,
+            loop: true,
+        };
+        this.typed = new Typed(this.el, options);
+    }
+
+    componentWillUnmount() {
+        this.typed.destroy();
+    }
+
     render() {
         return (
             <div className="landing-text-container">
                 <h1>Hello there, I'm Ashwin</h1>
+                <div className="type-wrap">
+                    <span
+                        style={{ whiteSpace: 'pre' }}
+                        ref={(el) => { this.el = el; }}
+                    />
+                </div>
             </div>
-        )
+        );
     }
 }
 

e91548e538301a5b64cdfdc591e891769205fb36
Ashwin Chatterji <ashwinath@hotmail.com>
2018-10-11 21:15:31 +0800

    feat: travis set up

--- scripts/build.sh
+++ scripts/build.sh
@@ -5,10 +5,11 @@ cd frontend
 rm -rf build/
 yarn
 yarn build
+cd ..
 
 # build container and push to dockerhub
 docker build -t ashwinath/personal-website-4 $* .
 docker login -u="ashwinath" -p="$DOCKER_PASSWORD"
 docker push ashwinath/personal-website-4:latest
 
-ssh -o "StrictHostKeyChecking no" travis@ashwinchat.com -t "docker pull ashwinath/personal-website-4; docker stop website-beta; docker rm website-beta; docker run --name website-beta -d -v /var/log/website-beta:/var/log/website-beta -p 8080:8080 ashwinath/personal-website-4; docker image prune -f"
+ssh -o "StrictHostKeyChecking no" travis@ashwinchat.com -t "docker pull ashwinath/personal-website-4; docker stop website-beta; docker rm website-beta; docker run --name website-beta -d -v /var/log/website-beta:/var/log/website-beta -p 23000:8080 ashwinath/personal-website-4; docker image prune -f"
